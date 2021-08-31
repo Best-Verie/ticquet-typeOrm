64a3cbf2c90b6ff60015bd56876622028b92810b
Best Verie Iradukunda <60257249+Best-Verie@users.noreply.github.com>
2021-08-31 17:10:07 +0200

    fit:users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/user.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: 'remotemysql.com',
       port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
-      entities: [],
+      username: 'mHKd2hlIRp',
+      password: '2pXn6Yye8B',
+      database: 'mHKd2hlIRp',
+      entities: [User],
       synchronize: true,
     }),
+    UserModule,
   ],
 })
 export class AppModule {}
